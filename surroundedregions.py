7f787db98531429551090010091c47888179a0d2
Daniel Smith <danielmsmith1@gmail.com>
2021-11-01 21:36:13 -0400

    soln and docs

--- surroundedregions.py
+++ surroundedregions.py
@@ -1,54 +1,44 @@
-#https://leetcode.com/problems/surrounded-regions/
+#https://leetcode.com/problems/surrounded-regions/submissions/
+#https://leetcode.com/problems/surrounded-regions/discuss/1553272/Explanation-using-Pictures-oror-DFS-oror-Python
 class Solution:
-    def isborder(m,n,x,y):
-        isb=False
-        if x == 0:
-            isb = True
-        elif x == m:
-            isb = True
-        elif y == 0:
-            isb = True
-        elif y == n:
-            isb = True
-        else:
-            isb = False
-        return isb   
     
-    def issurr(board,i,j):
-        sur = False
-        if board[i][j-1] =='X' and (board[i][j+1] =='X') and (board[i+1][j] =='X') and (board[i-1][j] =='X'):
-            return True
-
-        #check 4 directions North
-        if (Solution.issurr(board,i,j-1)):
-            #south
-            if  Solution.issurr(board,i,j+1):
-                print(i,j+1,board[i][j+1])
-                #east
-                if  Solution.issurr(board,i+1,j):
-                    print(i+1,j,board[i+1][j])
-                    #west
-                    if  Solution.issurr(board,i-1,j):
-                        return True
+    def depth_first_search(self, board, row, col):
         
-
-            
+        if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] in 'XV':
+            return 
+        
+        board[row][col] = 'V'
+        moves = [(1,0),(-1,0),(0,1),(0,-1)]
+        for dr,dc in moves:
+            self.depth_first_search(board, row + dr, col + dc)
+    
+    
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
 
-        m = len(board)-1
-        n = len(board[0])-1
-        print (m,n)
-        for i in range(0,m):
-            for j in range(0,n):
-                if not Solution.isborder(m,n,i,j):
-                    print(i,j,board[i][j],'notborder')
-                    if board[i][j]=='O':
-                        flip = False
-                        if Solution.issurr(board,i,j):
-                            flip = True
-                            board[i][j]='X'
-        print (board)
-                                        
+        rows_num,cols_num = len(board), len(board[0])
+        
+        # Traversing Grid along the 0th and (n-1)th row
+        for col in range(cols_num):
+            # 0th row
+            if board[0][col] == 'O':
+                self.depth_first_search(board, 0, col)
+            # (rows_num-1)th row
+            if board[rows_num-1][col] == 'O':
+                self.depth_first_search(board, rows_num-1, col)
+        
+        # Traversing Grid along the 0th and (m-1)th column
+        for row in range(rows_num):
+            # 0th column
+            if board[row][0] == 'O':
+                self.depth_first_search(board, row, 0)
+            # (m-1)th column
+            if board[row][cols_num-1] == 'O':
+                self.depth_first_search(board, row, cols_num-1)
+                
+        # post-processing board        
+        for row in range(rows_num):
+            for col in range(cols_num):
+                if board[row][col] == 'V':
+                    board[row][col] = 'O'
+                elif board[row][col] == 'O':
+                    board[row][col] = 'X'                
