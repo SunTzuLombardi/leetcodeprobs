0539e3acecb2d16125a4d03acd1ab024610bd293
Daniel Smith <danielmsmith1@gmail.com>
2021-11-08 19:32:22 -0500

    solution2

--- leetcode.py
+++ leetcode.py
@@ -1,14 +1,19 @@
 https://leetcode.com/problems/palindrome-number/solution/
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        import math
-        isp = False
-        if(x < 0 or (x % 10 == 0 and x != 0)):
+        #store a copy of this number
+        temp=x
+        #calculate reverse of this number
+        reverse_num=0
+        while(x>0):
+            #extract last digit of this number
+            digit=x%10
+            #append this digit in reveresed number
+            reverse_num=reverse_num*10+digit
+            #floor divide the number leave out the last digit from number
+            x=x//10
+        #compare reverse to original number
+        if(temp==reverse_num):
+            return True
+        else:
             return False
-
-        rn = 0
-        while x > rn :
-            rn = rn * 10 + x %10
-            x /=10
-
-        return x == rn or x == rn/10
