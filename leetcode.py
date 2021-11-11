d31252cc3fc810c77f90d11fd49678cfef4d02bd
Daniel Smith <danielmsmith1@gmail.com>
2021-11-11 01:16:10 -0500

    linkedlist2

--- leetcode.py
+++ leetcode.py
@@ -84,3 +84,38 @@ class Solution:
 
 
         return merged
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    
+    
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        merged = ListNode()
+        tail = merged   
+        
+        #if both None return None
+        if l1 is None and l2 is None:
+            return None
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+        #if l1 not None
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2 
+ 
+        return merged.next
